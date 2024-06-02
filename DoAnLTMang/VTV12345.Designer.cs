d983f90949211be783475058e5395b0573f00122
ThienAnUIT <22520028@gm.uit.edu.vn>
2024-06-02 18:21:01 +0700

    finish get schedule of VTV1,2,3,4,5 chanels

---
+++ DoAnLTMang/VTV12345.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System.Windows.Forms;
+
+namespace DoAn
+{
+    partial class VTV12345
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VTV12345));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 190);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(21, 3);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1275, 197);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 31);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1267, 162);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "VTV1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1255, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 31);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1267, 162);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "VTV2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Location = new System.Drawing.Point(6, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1255, 150);
+            this.panel2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.panel3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 31);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1267, 162);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "VTV3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.Location = new System.Drawing.Point(6, 6);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1255, 150);
+            this.panel3.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.panel4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 31);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1267, 162);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "VTV4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.Location = new System.Drawing.Point(6, 6);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1255, 150);
+            this.panel4.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.panel5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 31);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1267, 162);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "VTV5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.AutoScroll = true;
+            this.panel5.Location = new System.Drawing.Point(6, 6);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1255, 150);
+            this.panel5.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(481, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 139);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // VTV12345
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 392);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "VTV12345";
+            this.Text = "VTV12345";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+    }
+}
---
+++ DoAnLTMang/VTV12345.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static DoAn.BetaCine;
+
+namespace DoAn
+{
+    public partial class VTV12345 : Form
+    {
+        private HtmlAgilityPack.HtmlDocument document;
+        List<Show> Shows1, Shows2, Shows3, Shows4, Shows5;
+        private WebClient myClient;
+
+        public class Show
+        {
+            public string Link_Uri { get; set; }
+            public string Show_Title { get; set; }
+            public string Show_Time { get; set; }
+            public string Show_Genre { get; set; }
+        }
+
+        public VTV12345()
+        {
+            InitializeComponent();
+            Load_Shows();
+        }
+
+        private void Load_Shows()
+        {
+            myClient = new WebClient();
+            myClient.Encoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri("https://vtv.vn/lich-phat-song.htm");
+            try
+            {
+                Stream replied = myClient.OpenRead(uri);
+                string html = myClient.DownloadString(uri);
+                document = new HtmlAgilityPack.HtmlDocument();
+                document.LoadHtml(html);
+                replied.Close();
+                myClient.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi kết nối", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (document == null) return;
+
+            PopulateChannelPanel(Shows1 = GetShowsForChannel(1), panel1);
+            PopulateChannelPanel(Shows2 = GetShowsForChannel(2), panel2);
+            PopulateChannelPanel(Shows3 = GetShowsForChannel(3), panel3);
+            PopulateChannelPanel(Shows4 = GetShowsForChannel(4), panel4);
+            PopulateChannelPanel(Shows5 = GetShowsForChannel(5), panel5);
+        }
+
+        private List<Show> GetShowsForChannel(int channelNumber)
+        {
+            List<Show> Shows = new List<Show>();
+            var showTimeNodes = document.DocumentNode.SelectNodes($"//*[@id=\"wrapper\"]/ul[{channelNumber}]/li/span[1]");
+            var showTitleNodes = document.DocumentNode.SelectNodes($"//*[@id=\"wrapper\"]/ul[{channelNumber}]/li/span[2]");
+            var showGenreNodes = document.DocumentNode.SelectNodes($"//*[@id=\"wrapper\"]/ul[{channelNumber}]/li/a");
+
+            if (showTimeNodes != null)
+            {
+                for (int i = 0; i < showTimeNodes.Count; i++)
+                {
+                    Shows.Add(new Show { Show_Time = showTimeNodes[i].InnerText });
+                }
+            }
+
+            if (showTitleNodes != null)
+            {
+                for (int i = 0; i < showTitleNodes.Count; i++)
+                {
+                    Shows[i].Show_Title = showTitleNodes[i].InnerText;
+                }
+            }
+
+            if (showGenreNodes != null)
+            {
+                int genreIndex = 0;
+                for (int i = 0; i < Shows.Count; i++)
+                {
+                    if (genreIndex < showGenreNodes.Count && showGenreNodes[genreIndex].ParentNode.SelectSingleNode($"span[1]").InnerText == Shows[i].Show_Time)
+                    {
+                        Shows[i].Show_Genre = showGenreNodes[genreIndex].InnerText;
+                        genreIndex++;
+                    }
+                    else
+                    {
+                        Shows[i].Show_Genre = "";
+                    }
+                }
+            }
+
+            // Ensure Show_Genre is not null
+            foreach (var show in Shows)
+            {
+                if (string.IsNullOrWhiteSpace(show.Show_Genre))
+                {
+                    show.Show_Genre = "";
+                }
+            }
+
+            return Shows;
+        }
+
+        private void PopulateChannelPanel(List<Show> Shows, Panel DisplayPanel)
+        {
+            DisplayPanel.Controls.Clear();
+            int xPosition = 0;
+
+            foreach (var show in Shows)
+            {
+                Panel panel = new Panel
+                {
+                    Width = 180, // Adjust width
+                    Height = 70, // Adjust height
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Location = new Point(xPosition, 0),
+                    Margin = new Padding(5)
+                };
+
+                Label lblTime = new Label
+                {
+                    Text = show.Show_Time,
+                    AutoSize = true,
+                    Location = new Point(10, 10)
+                };
+                panel.Controls.Add(lblTime);
+
+                Label lblTitle = new Label
+                {
+                    Text = show.Show_Title,
+                    AutoSize = true,
+                    Location = new Point(10, 30),
+                    Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)))
+                };
+                panel.Controls.Add(lblTitle);
+
+                LinkLabel lblGenre = new LinkLabel
+                {
+                    Text = show.Show_Genre,
+                    AutoSize = true,
+                    Location = new Point(10, 50),
+                    Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)))
+                };
+                panel.Controls.Add(lblGenre);
+
+                DisplayPanel.Controls.Add(panel);
+                xPosition += panel.Width + 10;
+            }
+        }
+    }
+}
