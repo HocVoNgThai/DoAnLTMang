8479d65a3ea1f2ec49b625126ae290c962b38ee0
YeonBi01 <vonhathoangg@gmail.com>
2024-06-05 12:23:30 +0700

    cập nhật Detail_CPQG

--- DoAnLTMang/Detail_CPQG.cs
+++ DoAnLTMang/Detail_CPQG.cs
@@ -26,16 +26,18 @@ namespace DoAn
         {
             pictureBox1.ImageLocation = film.ImageUrl;
             label1.Text = film.FilmName;
-            label2.Text = film.Description;
+            label2.Text = film.Introduction;
             lbDaoDien.Text = film.Director;
             lbDienVien.Text = film.Actors;
             lbTheLoai.Text = film.Category;
             lbNgonNgu.Text = film.CountryName;
             lbThoiLuong.Text = film.Duration;
             Console.WriteLine(film.VideoUrl);
+            string video_Url=film.VideoUrl;
+            MessageBox.Show("Connect successful");
             if (webView21 != null && webView21.CoreWebView2 != null)
             {
-                webView21.CoreWebView2.Navigate(film.VideoUrl);
+                webView21.CoreWebView2.Navigate(video_Url);
             }
         }
     }
